bbc9df202664703d2edf802237277990f3e76ef4
Lorenzo Martínez <lorenzo.martinez@nunsys.com>
2022-03-08 16:38:17 +0100

    Mejorado el formato del ejercicio de NodeJS

--- docker/imagenes/app.js
+++ docker/imagenes/app.js
@@ -2,11 +2,18 @@ const http = require('http');
 
 const hostname = '0.0.0.0';
 const port = 3000;
+let contador = 1;
 
 const server = http.createServer((req, res) => {
+  const result = 
+`Hola Mundo
+(Invocado ${contador} ${contador > 1 ? 'veces' : 'vez'})
+`;
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('Hola Mundo\r\n');
+  res.end(result);
+  console.log(`Devolviendo el valor ${contador}...`);
+  contador++;
 });
 
 server.listen(port, hostname, () => {
